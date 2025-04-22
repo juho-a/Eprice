aeeba7fe46fcc08c6aa1ab33b7cb850fd8c16c19
PaavoReinikka <paavo.reinikka@proton.me>
2025-04-22 13:55:15 +0300

    ChatBot update (CHAT_URL)

--- App/client/src/lib/components/ChatBot.svelte
+++ App/client/src/lib/components/ChatBot.svelte
@@ -1,10 +1,12 @@
 <script>
+    import { PUBLIC_CHAT_URL } from "$env/static/public";
+    
     let chatOnState = $state(false);
-    //let data = $props(); WE CAN GET THE USER INFO THIS WAY
     const toggleChat = () => {
         chatOnState = !chatOnState;
     };
     let user = $props();
+
 </script>
 
 <!-- Styles like chat-toggle are in app.css (mix with tailwind) -->
@@ -17,7 +19,7 @@
 {#if chatOnState}
     <div class="gradio-embed" style="position: fixed; bottom: 0; right: 0; width: 400px; height: 800px; z-index: 9999;">
         <iframe title="Gradio App"
-            src="http://localhost:7860/"
+            src="{PUBLIC_CHAT_URL}"
             width="100%"
             height="100%"
             frameborder="20"
