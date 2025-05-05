47c7db71165c6f4b73b34b43383366cc6a9d6406
PaavoReinikka <paavo.reinikka@proton.me>
2025-05-05 16:06:49 +0300

    demo image added

---
+++ App/client/src/lib/components/ChartExample.svelte
@@ -0,0 +1,57 @@
+<script>
+	import chartjs from 'chart.js/auto';
+	import { onMount } from 'svelte';
+	import { usePricesState } from '$lib/states/usePricesState.svelte';
+
+
+	// THIS IS THE CHART EXAMPLE 3
+	// here we are using the prices state to get the data for the chart
+	// the state can be updated when needed (prices.update)
+	let prices = usePricesState();
+	let ctx;
+	let chartCanvas;
+
+	onMount(async (promise) => {
+        // trick to force the stateful data into standard javascript object
+		// this is a workaround for the fact that svelte stores are not serializable
+		const chartData = prices.state2js();
+
+		
+		ctx = chartCanvas.getContext('2d');
+		var chart = new chartjs(ctx, {
+				type: chartData.chartType,
+				data: {
+						labels: chartData.chartLabels,
+						datasets: [{
+								label: chartData.chartLegend,
+								backgroundColor: 'rgb(70, 50, 255)',
+								borderColor: 'rgb(255, 255, 255)',
+								data: chartData.chartValues,
+						}]
+				},
+				options: {
+					responsive: true,
+					maintainAspectRatio: true,
+					scales: {
+						x: {
+							ticks: {
+								maxRotation: 60,
+								minRotation: 60
+							}
+						}
+					}
+				}
+		});
+
+	});
+</script>
+
+<canvas bind:this={chartCanvas} id="myChart" class="
+	absolute
+	bottom-10
+	top-10
+	m-auto
+	rounded"
+ style="width: 100%;background-color: #1e1e2f;"
+></canvas>
+
